de8dc36e45c80f332189a838dacd73979d2b841a
Nier <nier@mexc.com>
2025-04-27 09:06:01 +0800

    更新油猴插件默认样式

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 // 挂载节点
 const monkey_plugin = document.createElement("div")
 monkey_plugin.id = "monkey_plugin"
-monkey_plugin.style = "position:fixed;top:10px;left:10px;width:200px;height:200px;z-index:1000"
+monkey_plugin.style = "width:100%;z-index:1000"
 document.body.appendChild(monkey_plugin)
 
 createApp(App).mount("#monkey_plugin")
